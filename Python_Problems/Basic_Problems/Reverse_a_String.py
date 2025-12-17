49da982a224f94ce2ea57cf7d233848eb7b015ce
PalaniGuna-123 <gunaguna20994@gmail.com>
2025-12-17 17:52:22 +0530

    complete reverse a string problem in python

---
+++ Python_Problems/Basic_Problems/Reverse_a_String.py
@@ -0,0 +1,7 @@
+# Reverse a string problem write a function that returns the reverse of a given string.
+# Example: input "hello" → output "olleh"
+
+def reverse_string(S: str) -> str:
+    return S[::-1]
+
+print(reverse_string("Guna Vision"))

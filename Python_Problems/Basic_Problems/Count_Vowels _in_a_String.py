12abace2e3cdb89c5528f5c15eaf92583d145331
PalaniGuna-123 <gunaguna20994@gmail.com>
2025-12-17 18:51:09 +0530

    even number odd number problem

--- Python_Problems/Basic_Problems/Count_Vowels _in_a_String.py
+++ Python_Problems/Basic_Problems/Count_Vowels _in_a_String.py
@@ -6,3 +6,13 @@ def count_Vowels(S: str) -> int:
             count +=1
     return count
 print(count_Vowels("Guna Vision"))
+
+def count_Vowels_with_letter(S: str) -> str:
+    vowels ="AEIOUaeiou"
+    Str=""
+    for char in S:
+        if char in vowels:
+            Str +=char
+    return Str + " : " + str(len(Str))
+
+print(count_Vowels_with_letter("Guna Vision"))
---
+++ Python_Problems/Basic_Problems/check_even_or_odd.py
@@ -0,0 +1,7 @@
+def check_even_or_Odd(number: int) -> str:
+    if number % 2 == 0:
+        return f"{number} is a Even number"
+    else:
+        return f"{number} is a Odd number"
+print(check_even_or_Odd(10))
+print(check_even_or_Odd(7))

61891300f60f76754f9cd3b8116a6a5e8e620a24
PalaniGuna-123 <gunaguna20994@gmail.com>
2025-12-17 18:02:04 +0530

    problem solving understand comments added

--- Python_Problems/Basic_Problems/Reverse_a_String.py
+++ Python_Problems/Basic_Problems/Reverse_a_String.py
@@ -5,3 +5,18 @@ def reverse_string(S: str) -> str:
     return S[::-1]
 
 print(reverse_string("Guna Vision"))
+
+
+#  (s: str)
+
+# s → parameter (input variable)
+
+# : → type hint
+
+# str → means s should be a string
+
+# -> str :
+
+# This is a return type hint
+
+# It means the function will return a string value

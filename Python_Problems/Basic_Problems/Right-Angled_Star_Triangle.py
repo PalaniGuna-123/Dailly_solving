7db3a0f5cdc9696f4a3173791f3df4d68d82bb24
PalaniGuna-123 <gunaguna20994@gmail.com>
2025-12-18 18:22:13 +0530

    right angled star triangle pattern

---
+++ Python_Problems/Basic_Problems/Right-Angled_Star_Triangle.py
@@ -0,0 +1,11 @@
+# Right-Angled Star Triangle
+# *
+# **
+# ***
+# ****
+# *****
+
+def right_angled_star_triangle(n: int) -> None:
+    for i in range(1,n+1):
+        print("*"*i)
+right_angled_star_triangle(5)

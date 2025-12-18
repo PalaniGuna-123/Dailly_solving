6b2ba2c2934acbd7e4965203060e383057e60ed4
PalaniGuna-123 <gunaguna20994@gmail.com>
2025-12-18 16:02:36 +0530

    slicing concepts

--- Python_Problems/Basic_Problems/sum_of_num.py
+++ Python_Problems/Basic_Problems/sum_of_num.py
@@ -1,3 +1,6 @@
+from operator import index
+
+
 def sum_of_numbers(number: int) -> int:
     total =0
     for i in range(1,number+1): #Loop from 1 to n
@@ -6,3 +9,7 @@ def sum_of_numbers(number: int) -> int:
 
 print(sum_of_numbers(5)) 
         
+
+text ="GunaVisionChannal"
+print(text[0:19:3])
+print(len(text))

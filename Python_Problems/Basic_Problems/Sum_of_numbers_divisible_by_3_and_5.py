4f1088c47e27d627700f3ff46ba118de30738c1d
PalaniGuna-123 <gunaguna20994@gmail.com>
2025-12-18 13:32:05 +0530

    sum of numbers divisible by three and five

---
+++ Python_Problems/Basic_Problems/Sum_of_numbers_divisible_by_3_and_5.py
@@ -0,0 +1,11 @@
+def sum_of_numbers_divisible_by_3_and_5(number1: int , number2: int) -> int:
+    sum=0
+    for i in range(number1,number2 +1):
+        if i % 3 == 0 and i % 5 == 0:
+            sum +=i
+    return sum
+
+print(sum_of_numbers_divisible_by_3_and_5(3,90))
+        
+
+

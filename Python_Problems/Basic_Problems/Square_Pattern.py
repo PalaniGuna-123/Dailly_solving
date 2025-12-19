7a9c70d6bb9cd6edbe6ad38dd6c2df86c82399e5
PalaniGuna-123 <gunaguna20994@gmail.com>
2025-12-19 11:09:29 +0530

    square pattern

---
+++ Python_Problems/Basic_Problems/Square_Pattern.py
@@ -0,0 +1,15 @@
+def square_pattern(rows: int) -> None:
+    for i in range(rows):
+        print("*" * rows)
+
+square_pattern(5)
+
+# | Part | Meaning                        |
+# | ---- | ------------------------------ |
+# | `rows` | Number of rows in the square pattern |
+# | `*`  | Character to print             |         
+
+def square(rows: int) -> None:
+    for i in range(rows):
+        print("*" * rows )
+square(8)

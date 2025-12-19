6f1884d42d32bbe6bbe2d8c4f01f884ec5e95ea4
PalaniGuna-123 <gunaguna20994@gmail.com>
2025-12-19 12:12:48 +0530

    diamond pattern use j

--- Python_Problems/Interview_Point_Of_View/Diamond_Pattern.py
+++ Python_Problems/Interview_Point_Of_View/Diamond_Pattern.py
@@ -7,3 +7,12 @@ def Diamond_Pattern(n: int) -> None:
         print(" "*(n-i) + "*" *(2 * i -1))
 
 Diamond_Pattern(5)
+
+
+def Diamond_Pattern2(n: int) -> None:
+    for i in range(1,n+1):
+        print(" "*(n-i) + "*" * (2 * i -1))
+    for j in range(n-1,0,-1):
+        print(" " * (n -j) + "*" *(2 * j- 1))
+    
+Diamond_Pattern2(5)

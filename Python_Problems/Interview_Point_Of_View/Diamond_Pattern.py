5c139d6730b844f5a1ccbf4f1d6ffbeefd0b2ac7
PalaniGuna-123 <gunaguna20994@gmail.com>
2025-12-19 11:33:08 +0530

    Diamond Pattern problem

---
+++ Python_Problems/Interview_Point_Of_View/Diamond_Pattern.py
@@ -0,0 +1,9 @@
+def Diamond_Pattern(n: int) -> None:
+    # upper part
+    for i in range(1,n+1):
+        print(" "*(n - i ) + "*" * (2 * i -1))
+    #lower part
+    for i in range(n-1,0,-1):
+        print(" "*(n-i) + "*" *(2 * i -1))
+
+Diamond_Pattern(5)

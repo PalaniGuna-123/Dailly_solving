8e09e4a0d54945bf7b81d8a7b504a3c890acbf22
PalaniGuna-123 <gunaguna20994@gmail.com>
2025-12-22 16:07:08 +0530

    remove duplicate string

---
+++ Python_Problems/Interview_Point_Of_View/remove_duplicates_str.py
@@ -0,0 +1,23 @@
+def remove_duplicate_str(s):
+    result = []
+
+    for x in s:
+        found = False
+
+        for y in result:
+            if x == y:
+                found = True
+
+        if not found:
+            result.append(x)
+
+    # convert list back to string
+    final_str = ""
+    for ch in result:
+        final_str = final_str + ch
+
+    return final_str
+
+
+text = "mississippi"
+print(remove_duplicate_str(text))

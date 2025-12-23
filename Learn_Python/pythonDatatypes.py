0acab2206e0f2ab39ef6a5a3ce6f2faa808d833f
PalaniGuna-123 <gunaguna20994@gmail.com>
2025-12-23 11:21:01 +0530

    teach python datatypes

---
+++ Learn_Python/pythonDatatypes.py
@@ -0,0 +1,45 @@
+#Int
+# 10,20,30,40,50 
+age=24
+
+print(age)
+print(type(age))
+
+#Float
+# 10.5,20.5,30.5,40.5,50
+height=5.9
+
+print(height)
+print(type(height))
+
+#String
+# "Hello", "Python", "Programming"
+name="John Doe"
+
+print(name)
+print(type(name))
+
+#Boolean
+# True, False
+is_student=False
+print(is_student)
+print(type(is_student)) 
+
+
+#List
+# [10,20,30,40,50]["Hello", "Python", "Programming"]
+fruits=["Apple", "Banana", "Cherry"]
+print(fruits)
+print(type(fruits))
+
+#Tuple
+# (10,20,30,40,50)("Hello", "Python", "Programming")
+colors=("Red", "Green", "Blue")
+print(colors)
+print(type(colors))
+
+#dictionary
+# {"name": "John", "age": 24, "height": 5.9}
+person = {"name": "John", "age": 24, "height": 5.9}
+print(person)
+print(type(person))
